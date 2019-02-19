e5e41f09e24810eca5a0078f82f9718a5e2feb44
MAMAMA <42968079+mamamaisused@users.noreply.github.com>
2019-02-19 15:10:49 +0800

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,175 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.txtInput = New System.Windows.Forms.TextBox()
+        Me.txtInputBinary = New System.Windows.Forms.TextBox()
+        Me.txtOutpuBinary = New System.Windows.Forms.TextBox()
+        Me.txtInputHex = New System.Windows.Forms.TextBox()
+        Me.txtOutputHex = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(206, 362)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(137, 54)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Change"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'txtInput
+        '
+        Me.txtInput.Font = New System.Drawing.Font("宋体", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.txtInput.Location = New System.Drawing.Point(206, 64)
+        Me.txtInput.Name = "txtInput"
+        Me.txtInput.Size = New System.Drawing.Size(148, 35)
+        Me.txtInput.TabIndex = 1
+        '
+        'txtInputBinary
+        '
+        Me.txtInputBinary.Enabled = False
+        Me.txtInputBinary.Font = New System.Drawing.Font("微软雅黑", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.txtInputBinary.Location = New System.Drawing.Point(305, 187)
+        Me.txtInputBinary.Name = "txtInputBinary"
+        Me.txtInputBinary.Size = New System.Drawing.Size(148, 39)
+        Me.txtInputBinary.TabIndex = 1
+        '
+        'txtOutpuBinary
+        '
+        Me.txtOutpuBinary.Enabled = False
+        Me.txtOutpuBinary.Font = New System.Drawing.Font("微软雅黑", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.txtOutpuBinary.Location = New System.Drawing.Point(305, 238)
+        Me.txtOutpuBinary.Name = "txtOutpuBinary"
+        Me.txtOutpuBinary.Size = New System.Drawing.Size(148, 39)
+        Me.txtOutpuBinary.TabIndex = 1
+        '
+        'txtInputHex
+        '
+        Me.txtInputHex.Enabled = False
+        Me.txtInputHex.Font = New System.Drawing.Font("微软雅黑", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.txtInputHex.Location = New System.Drawing.Point(110, 187)
+        Me.txtInputHex.Name = "txtInputHex"
+        Me.txtInputHex.Size = New System.Drawing.Size(148, 39)
+        Me.txtInputHex.TabIndex = 1
+        '
+        'txtOutputHex
+        '
+        Me.txtOutputHex.Enabled = False
+        Me.txtOutputHex.Font = New System.Drawing.Font("微软雅黑", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.txtOutputHex.Location = New System.Drawing.Point(110, 238)
+        Me.txtOutputHex.Name = "txtOutputHex"
+        Me.txtOutputHex.Size = New System.Drawing.Size(148, 39)
+        Me.txtOutputHex.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("宋体", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.Label1.Location = New System.Drawing.Point(12, 190)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(70, 24)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Input"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("宋体", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.Label2.Location = New System.Drawing.Point(12, 241)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(82, 24)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Output"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("宋体", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.Label3.Location = New System.Drawing.Point(155, 144)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(46, 24)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Hex"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("宋体", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.Label4.Location = New System.Drawing.Point(339, 144)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(46, 24)
+        Me.Label4.TabIndex = 2
+        Me.Label4.Text = "Bin"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("宋体", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.Label5.Location = New System.Drawing.Point(70, 64)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(118, 24)
+        Me.Label5.TabIndex = 2
+        Me.Label5.Text = "Input Dec"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 18.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(546, 442)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtOutputHex)
+        Me.Controls.Add(Me.txtOutpuBinary)
+        Me.Controls.Add(Me.txtInputHex)
+        Me.Controls.Add(Me.txtInputBinary)
+        Me.Controls.Add(Me.txtInput)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents txtInput As System.Windows.Forms.TextBox
+    Friend WithEvents txtInputBinary As System.Windows.Forms.TextBox
+    Friend WithEvents txtOutpuBinary As System.Windows.Forms.TextBox
+    Friend WithEvents txtInputHex As System.Windows.Forms.TextBox
+    Friend WithEvents txtOutputHex As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,52 @@
+﻿''' <summary>
+''' 单个字节二进制MSB开头转换为LSB开头
+''' </summary>
+''' <remarks></remarks>
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If txtInput.Text = "" Then
+            txtInput.Text = 1
+        End If
+        '二进制显示
+        txtInputBinary.Text = Convert.ToString(Convert.ToByte(txtInput.Text), 2)
+        '前段补零
+        FormatBin(txtInputBinary.Text)
+        '十六进制显示2位
+        txtInputHex.Text = Convert.ToByte(txtInput.Text).ToString("X2")
+
+        Dim res As Byte
+        '二进制数据颠倒
+        txtOutpuBinary.Text = InvertBin(txtInputBinary.Text)
+        '二进制字符串转换为Byte型
+        res = Convert.ToByte(txtOutpuBinary.Text, 2)
+        '十六进制显示
+        txtOutputHex.Text = res.ToString("X2")
+
+    End Sub
+
+    Public Sub FormatBin(ByRef str As String)
+        Dim len As Integer
+        len = str.Length
+        If len < 8 Then
+            For i As Integer = 1 To 8 - len
+                str = "0" + str
+            Next
+        End If
+    End Sub
+
+    Public Function InvertBin(_byte As String) As String
+        Debug.Print("Start:")
+        Dim bit(8) As String
+        Dim tmp As String
+        tmp = _byte
+        For i As Integer = 0 To 7
+            bit(i) = Mid(tmp, i + 1, 1)
+            Debug.Print(bit(i))
+        Next
+        tmp = ""
+        For j As Integer = 7 To 0 Step -1
+            tmp = tmp + bit(j)
+        Next
+        Return tmp
+    End Function
+End Class
